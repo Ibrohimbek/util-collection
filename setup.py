5c6d9807aad12a8a53d8eb4a5f6a65056f42ebdd
Ibrohim Ermatov <ibrohim@mysuperdispatch.com>
2018-10-31 18:45:42 +0500

    Create setup file with package details

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="djangojsendresponses",
+    version="0.1.1",
+    url="https://github.com/Ibrohimbek/django-jsend-responses",
+    author="Ibrohim Ermatov",
+    author_email="ibrohimbek@gmail.com",
+    description="Util methods to use in projects",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
